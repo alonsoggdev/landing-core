6fd9d39611000b24b8074257d2954e650420ccf0
Alonso García <167032274+alonsoggdev@users.noreply.github.com>
2026-02-23 13:25:55 +0100

    Update BaseController.php

--- src/Core/BaseController.php
+++ src/Core/BaseController.php
@@ -16,10 +16,11 @@ class BaseController extends Controller
         $this->seo = new SEOService();
     }
 
-    protected function render(string $view, array $data = [])
+    protected function render(string $view, array $data = [], string $layout = 'front')
     {
         $data['seo'] = $this->seo;
+        $data['content'] = view($view, $data);
 
-        return view($view, $data);
+        return view("layouts/{$layout}", $data);
     }
 }
